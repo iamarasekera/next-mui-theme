0650fae337a1f9559710a7b2339358bc3d43e2ed
iamarasekera <iamarasekera.dev@gmail.com>
2024-10-17 11:52:00 +1030

    Configured the project for aws deployement

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
+    assetPrefix: 'https://d1dao37twwf25.cloudfront.net',
+    images: {
+      unoptimized: true, // Disable image optimization in export mode
+    },
+    experimental: {
+      outputFileTracingRoot: undefined,
+    },
+  }
+  
+  module.exports = nextConfig
 
-module.exports = nextConfig
